9e7a55470ba81184af75748903fe039a24512585
ALEXISBRUNO-code <velardealeno18@autonoma.edu.pe>
2025-09-30 19:36:37 -0500

    CARGA INICIAL

---
+++ src/Programas/Prueba.java
@@ -0,0 +1,13 @@
+
+package Programas;
+
+
+public class Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Alexis");
+        System.out.println("Mi edad es ....");
+        System.out.println("Mi generi es Masculino");
+    }
+    
+}
