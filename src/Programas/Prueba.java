ab94ed81444b3bfbc9a99f94ef19ebd35421ea8b
ALEXISBRUNO-code <velardealeno18@autonoma.edu.pe>
2025-09-30 20:06:00 -0500

    CARGA INICIAL

--- src/Programas/Prueba.java
+++ src/Programas/Prueba.java
@@ -9,6 +9,7 @@ public class Prueba {
         System.out.println("Mi edad es ....");
         System.out.println("Mi generi es Masculino");
         System.out.println("Mi correo es alexis18@gmail.com");
+        System.out.println("Mi telefono es 937224772");
     }
     
 }
